25d8afa70a45cfaab8b275fb4f4455abe1840fcf
Peter.K.Tseng <Peter.K.Tseng@benq.com>
2025-11-13 14:43:39 +0800

    feat: Add second set of intrinsic camera parameters

--- app/src/main/java/dev/app/tof/ToFIntrinsics.kt
+++ app/src/main/java/dev/app/tof/ToFIntrinsics.kt
@@ -5,4 +5,9 @@ object ToFIntrinsics {
     const val FY = 112.16f
     const val CX = 55.62f
     const val CY = 45.48f
+
+    const val FX2 =111f
+    const val FY2 = 111f
+    const val CX2 = 55.83f
+    const val CY2= 41.0f
 }
